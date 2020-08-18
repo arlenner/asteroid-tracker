85dd92f89a9a806017436a5ad9a2fc946808c5f1
Alexander <arlenross02@gmail.com>
2020-08-18 00:52:15 -0400

    Updates

---
+++ precache-manifest.dca8c59afbf7823434adfa1f3592332c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530bc4e0fb519633c7ccc9de49fa62ae",
+    "url": "/asteroid-tracker/index.html"
+  },
+  {
+    "revision": "be7b0ac44cd282d25ebc",
+    "url": "/asteroid-tracker/static/css/main.07af5152.chunk.css"
+  },
+  {
+    "revision": "779ad24f3655b70fc40b",
+    "url": "/asteroid-tracker/static/js/2.60776af0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asteroid-tracker/static/js/2.60776af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7b0ac44cd282d25ebc",
+    "url": "/asteroid-tracker/static/js/main.4477d666.chunk.js"
+  },
+  {
+    "revision": "2aa33e3e9cfa5300f978",
+    "url": "/asteroid-tracker/static/js/runtime-main.85118684.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/asteroid-tracker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fe5e7076427ec9305f6de18ecece4e3c",
+    "url": "/asteroid-tracker/static/media/starry-sky1.fe5e7076.jpg"
+  }
+]);
